e0df7ffa384c0fb98252acca4e72baff18ac5fb1
r4jdip <rajdipsaha.kv@gmail.com>
2020-08-29 14:04:28 +0530

    Update zoom-auto-join-scheduler.py

--- zoom-auto-join-scheduler.py
+++ zoom-auto-join-scheduler.py
@@ -3,33 +3,42 @@ from urllib import parse
 import threading
 import subprocess
 import os
+import winreg
 
-
+#function to join the meeting
 def startMeeting():
+
+    #forming the abs path to zoom bin
     pathToAppData = os.getenv('APPDATA')
     absPathToZoomBin = pathToAppData + "\\Zoom\\bin\\Zoom.exe"
+    #forming the arg to pass to zoom bin
     argToPass = '--url="'+zoommtgURL+'"'
+    #merging the abs path and arg to form command
     fullCommand = absPathToZoomBin+" "+argToPass
+    #supressing the output
     ONULL = open(os.devnull, 'w')
+    #calling the command
     subprocess.call(fullCommand, stdout=ONULL, stderr=ONULL, shell=False)
 
-#User Input
+
+    
+
+#user Input
 meetingURL = input("Enter the full meeting URL here: ").strip()
 scheduledAt = input("Enter the date & time when the meeting is scheduled in DD-MM-YYYY HH:MM format (Ex: 28-08-2020 21:00): ").strip()
 joinAs = input("Enter a name to join the meeting as: ").strip()
-parsed = parse.urlsplit(meetingURL)
 
+#parsing the URL into componenets
+parsed = parse.urlsplit(meetingURL)
 hashedMeetingPwd = parse.parse_qs(parsed.query)['pwd'][0]
 meetingID = parsed.path.split("/")[-1]
+
+#Parsing the date from str to datetime object
+scheduledAt = datetime.strptime(scheduledAt, '%d-%m-%Y %H:%M')
 
 #generating the new url which has zoommtg protocol (will be used to pass as argument to zoom.exe)
 zoommtgURL = "zoommtg://us04web.zoom.us/join?action=join&confno="+meetingID+"&pwd="+hashedMeetingPwd+"&uname="+joinAs
 
-
-
-
-#Parsing the date from str to datetime object
-scheduledAt = datetime.strptime(scheduledAt, '%d-%m-%Y %H:%M')
 #scheduling the meeting
 now = datetime.now()
 delayInSecs = (scheduledAt - now).total_seconds()
