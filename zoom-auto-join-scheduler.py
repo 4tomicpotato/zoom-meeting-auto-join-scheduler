0a600a997b1d2e3fb986425b6130501b78fdc6ea
r4jdip <rajdipsaha.kv@gmail.com>
2020-08-29 15:33:51 +0530

    Update zoom-auto-join-scheduler.py

--- zoom-auto-join-scheduler.py
+++ zoom-auto-join-scheduler.py
@@ -34,25 +34,57 @@ def startRecording():
     #calling the command to record
     subprocess.Popen(commandToStartRecording, stdout=ONULL, stderr=ONULL, shell=True)
     
+while 1:
+    #user Input
+    meetingURL = input("Enter the full zoom meeting URL here: ").strip()
+
+    #parsing the URL into componenets
+    parsed = parse.urlsplit(meetingURL)
+    #not a zoom meeting
+    if "zoom.us" not in parsed.netloc:
+        print("Invalid URL input. Your meeting URL should look like: https://us04web.zoom.us/j/XXXXXXXXXX?pwd=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX. Try again.")
+        continue
+    
+    #checking the validity of input url
+    hashedMeetingPwd = parse.parse_qs(parsed.query)['pwd'][0]
+    if(len(hashedMeetingPwd) != 32):
+        print("Invalid pwd part of URL. Your meeting URL should look like: https://us04web.zoom.us/j/XXXXXXXXXX?pwd=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX. Try again.")
+        continue
+
+    meetingID = parsed.path.split("/")[-1]
+    if(not(meetingID.isdecimal())):
+        print("Invalid ID part of URL. Your meeting URL should look like: https://us04web.zoom.us/j/XXXXXXXXXX?pwd=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX. Try again.")
+        continue
+
+    break
+
+while 1:
+    #user Input
+    scheduledAt = input("Enter the date & time when the meeting is scheduled in DD-MM-YYYY HH:MM format (Ex: 28-08-2020 21:00): ").strip()
+
+    #Parsing the date from str to datetime object & error-handling
+    try: 
+        scheduledAt = datetime.strptime(scheduledAt, '%d-%m-%Y %H:%M')
+    except:
+        print("Invalid date-time input. It should be in DD-MM-YYYY HH:MM format. Try again.")
+        continue
+    
+    #checking the validity of input date-time
+    now = datetime.now()
+    if(now > scheduledAt):
+        print("Invalid Date & Time input. Scheduled date-time can't be lower than current date-time. Try again.")
+        continue
+    break
+
 
 #user Input
-meetingURL = input("Enter the full meeting URL here: ").strip()
-scheduledAt = input("Enter the date & time when the meeting is scheduled in DD-MM-YYYY HH:MM format (Ex: 28-08-2020 21:00): ").strip()
 joinAs = input("Enter a name to join the meeting as: ").strip()
-recordOption = input("Do you want to record your meeting? [y/N]").strip()
-
-#parsing the URL into componenets
-parsed = parse.urlsplit(meetingURL)
-hashedMeetingPwd = parse.parse_qs(parsed.query)['pwd'][0]
-meetingID = parsed.path.split("/")[-1]
-
-#Parsing the date from str to datetime object
-scheduledAt = datetime.strptime(scheduledAt, '%d-%m-%Y %H:%M')
+#user Input
+recordOption = input("Do you want to record your meeting (Bandicam required) ? [y/N]").strip()
 
 #generating the new url which has zoommtg protocol (will be used to pass as argument to zoom.exe)
 zoommtgURL = "zoommtg://us04web.zoom.us/join?action=join&confno="+meetingID+"&pwd="+hashedMeetingPwd+"&uname="+joinAs
 
 #scheduling the meeting
-now = datetime.now()
 delayInSecs = (scheduledAt - now).total_seconds()
 threading.Timer(delayInSecs, startMeeting).start()
