b3029fd22167fafaac1861508f4a3b357d96a632
4tomicpotato <49423430+4tomicpotato@users.noreply.github.com>
2020-09-20 19:20:12 +0530

    fixed the download folder locating code

--- modules/common_funcs_lib.py
+++ modules/common_funcs_lib.py
@@ -236,7 +236,7 @@ def initializeBandicamSetup(firstTime = False):
                 executeCommand("REG ADD HKCU\\" + regPathToOptions + " /v " + key2Name + " /t REG_DWORD /d 1 /f", True)
                 
         except:
-            print("Failed to configure Bandicam recording options. Manually open Bandicam & set recording option to full screen.")
+            print("Make sure bandicam is set to fullscreen mode.")
         else:
             #confirming the values have changed
             regKey1Val = queryRegValue(regPathToOptions, key1Name)
@@ -306,5 +306,50 @@ def addTask(taskName, scheduleDateTime, pathToPythonExec, execModulePath, unique
     commandToSchedule = 'SCHTASKS /CREATE /TN "'+ taskName +'" /XML "'+ fullPath +'" /F'
 
     executeCommand(commandToSchedule, True)
+
+
+if os.name == 'nt':
+    import ctypes
+    from ctypes import windll, wintypes
+    from uuid import UUID
+
+    # ctypes GUID copied from MSDN sample code
+    class GUID(ctypes.Structure):
+        _fields_ = [
+            ("Data1", wintypes.DWORD),
+            ("Data2", wintypes.WORD),
+            ("Data3", wintypes.WORD),
+            ("Data4", wintypes.BYTE * 8)
+        ] 
+
+        def __init__(self, uuidstr):
+            uuid = UUID(uuidstr)
+            ctypes.Structure.__init__(self)
+            self.Data1, self.Data2, self.Data3, \
+                self.Data4[0], self.Data4[1], rest = uuid.fields
+            for i in range(2, 8):
+                self.Data4[i] = rest>>(8-i-1)*8 & 0xff
+
+    SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
+    SHGetKnownFolderPath.argtypes = [
+        ctypes.POINTER(GUID), wintypes.DWORD,
+        wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
+    ]
+
+    def _get_known_folder_path(uuidstr):
+        pathptr = ctypes.c_wchar_p()
+        guid = GUID(uuidstr)
+        if SHGetKnownFolderPath(ctypes.byref(guid), 0, 0, ctypes.byref(pathptr)):
+            raise ctypes.WinError()
+        return pathptr.value
+
+    FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+    def getDownloadFolder():
+        return _get_known_folder_path(FOLDERID_Download)
+else:
+    def getDownloadFolder():
+        home = os.path.expanduser("~")
+        return os.path.join(home, "Downloads")
 
     
